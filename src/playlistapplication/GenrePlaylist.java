89f1c7e52bceebd020cfd06904e446b1c329c16e
Yonas <116105802+yonas255@users.noreply.github.com>
2024-03-13 18:52:50 +0000

    creating a playlist application

--- src/playlistapplication/GenrePlaylist.java
+++ src/playlistapplication/GenrePlaylist.java
@@ -10,7 +10,44 @@ import java.util.List;
  *
  * @author yonas
  */
-public class GenrePlaylist implements PlayList  {
+public class GenrePlaylist implements PlayList {
     private String genre;
     private List<Song> songs; 
+
+    public GenrePlaylist(String genre, List<Song> songs) {
+        this.genre = genre;
+        this.songs = songs;
+    }
+    
+    
+
+    @Override
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public int size() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void addSong(Song song) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void removeSong(Song song) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Song searchSong(String title) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void print() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 }
