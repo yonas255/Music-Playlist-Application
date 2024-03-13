0820345a89422d6349b2db3a926bbaf72f6501fc
Yonas <116105802+yonas255@users.noreply.github.com>
2024-03-13 18:33:34 +0000

    creating a playlist application

---
+++ src/playlistapplication/GenrePlaylist.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package playlistapplication;
+
+import java.util.List;
+
+/**
+ *
+ * @author yonas
+ */
+public class GenrePlaylist implements PlayList  {
+    private String genre;
+    private List<Song> songs; 
+}
